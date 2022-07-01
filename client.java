fe31e19383bdb40583eda6368a9eee798a85da19
salma122110 <66427066+salma122110@users.noreply.github.com>
2022-07-01 05:14:48 +0200

    Update client.java

--- client.java
+++ client.java
@@ -41,3 +41,47 @@
         }
         return valid;
     }
+//////////////Function request for ViewAccountInformation////////////////////
+  public static String AccountInformationCS(String uname){
+    String data ="";
+    try{
+    dos.writeUTF("Account "+uname);
+    dos.flush();
+    data = dis.readUTF();
+    }
+    catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    return data;
+    }
+//////////////Function request for ShowClientList to Admin////////////////////
+  public static String getList(){
+      String clients="";
+        //out.println("getProductsList");
+        
+        try {
+            dos.writeUTF("getCLientList");
+            dos.flush();
+            //products=in.readLine();
+            clients=dis.readUTF();
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return clients;
+    }
+//////////////Function request to deposit money in E-wallet////////////////////
+    public static void depositCS (String uname,int balance,int depositedMoney)
+    {
+     //String data = "";
+    try{
+    dos.writeUTF("deposit "+uname+" "+balance+" "+depositedMoney);
+    dos.flush();
+    //out.println("deposit "+uname+" "+balance+" "+depositedMoney);
+  // data = dis.readUTF();
+    }
+    catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    //return data;
+    
+    }
