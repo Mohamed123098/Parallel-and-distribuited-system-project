09210f60c5a04f338b0e0bf1b718e2a274a2d391
Sandra-atef25 <85317706+Sandra-atef25@users.noreply.github.com>
2022-07-01 02:48:13 +0200

    Update client.java

--- client.java
+++ client.java
@@ -1,1 +1,43 @@
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//////////////Function request for Sign Up////////////////////
+   public static String signupCS(String uname,String pass,String address,int phone,String gender){
+        
+        out.println("signup "+uname+" "+pass+" "+address+" "+phone+" "+gender);
+        String valid="";
+        
+        try {
+            valid=in.readLine();
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return valid;
+    }
+
+//////////////Function request for Login////////////////////
+    public static String loginCS(String Role,String uname,String pass){
+        out.println("login "+Role+" "+uname+" "+pass);
+        
+        String valid="";
+         try {
+           
+            valid=in.readLine();
+
+        } catch (IOException ex) {
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return valid;
+    }
